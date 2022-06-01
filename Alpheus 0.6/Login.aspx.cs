543cc9ccbd58bdd586362eb562c1a34e71d10d7f
DavidGG12 <garcia.iandavid117@hotmail.com>
2022-06-01 11:36:51 -0500

    Login creado.

--- Alpheus 0.6/Login.aspx.cs
+++ Alpheus 0.6/Login.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Alpheus_0._6
 {
@@ -14,10 +17,45 @@ namespace Alpheus_0._6
 
         }
 
+        string patron = "InfoToolsSV";
         protected void boton_ingresar_Click(object sender, EventArgs e)
         {
-            Error.Text = "no";
+            string conectar = ConfigurationManager.ConnectionStrings["MatiasConnection"].ConnectionString;
+            SqlConnection con = new SqlConnection(conectar);
 
+            //Cambiar parámetros
+            SqlCommand cmd = new SqlCommand("Iniciar_Sesion", con)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            cmd.Connection.Open();
+            cmd.Parameters.Add("@usuario", SqlDbType.VarChar, 20).Value = Usuario.Text;
+            cmd.Parameters.Add("@contraseña", SqlDbType.VarChar, 20).Value = contraseña.Text;
+            cmd.Parameters.Add("@Patron", SqlDbType.VarChar, 50).Value = patron;
+
+            if (String.IsNullOrEmpty(Usuario.Text) || String.IsNullOrEmpty(contraseña.Text))
+            {
+                Error.Text = "Alguno de los campos está vacío.";
+            }
+            else
+            {
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    Error.Text = "Correo/contraseña inválidos";
+                }
+                else
+                {
+                    Error.Text = "Iniciado";
+                }
+            }
+
+            cmd.Connection.Close();
+        }
+
+        protected void boton_registrar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Registro_Usuario.aspx");
         }
     }
 }
